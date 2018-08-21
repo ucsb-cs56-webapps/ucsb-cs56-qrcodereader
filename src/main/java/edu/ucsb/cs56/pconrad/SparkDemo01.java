93ba50c16332678f1fee4131c46e3633cfbee0c7
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-21 16:47:18 -0700

    trying for Heroku

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -1,5 +1,8 @@
 package edu.ucsb.cs56.pconrad;
 
+import static spark.Spark.port;
+
+
 /**
  * Hello world!
  *
@@ -7,14 +10,29 @@ package edu.ucsb.cs56.pconrad;
 
 public class SparkDemo01 {
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+		
 		System.out.println("");
 		System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
 		System.out.println("");						  
-		System.out.println("In browser, visit: http://localhost:4567/hello");
+		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
 		System.out.println("");
-        spark.Spark.get("/hello", (req, res) -> "Hello World");
+        spark.Spark.get("/", (req, res) -> "Hello World");
+		spark.Spark.get("/hello", (req, res) -> "Hello World");
 		spark.Spark.get("/bye", (req, res) -> "Goodbye World");
 		spark.Spark.get("/yo", (req, res) -> "S'up World");
 		spark.Spark.get("/tension", (req, res) -> "Midterm next week.  No problem dude");		
+
+	}
+	
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
+
+	
 }
